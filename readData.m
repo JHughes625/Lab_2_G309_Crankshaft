28eb9651da692adbb7fb1a4c6fae3e4314bb0426
NicholasHamilton27 <nhamil1139@gmail.com>
2025-02-18 12:43:47 -0700

    Update readData.m
    updated for loops to read length of filename

--- readData.m
+++ readData.m
@@ -4,12 +4,12 @@
 function[dataStruct] = readData(filename)
 dataStruct = struct();
 
-for i = 1:6
+for i = 1:length(filename)
     dataStruct(i).filename = filename(i);
     dataStruct(i).data = readtable(filename(i));
 end
 
-for i = 1:6
+for i = 1:length(filename)
     for j = 2:height(dataStruct(i).data)
         if dataStruct(i).data.Var3(j) > dataStruct(i).data.Var3(j-1) && dataStruct(i).data.Var3(j) < 5
             start = j-1;
