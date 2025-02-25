62449d64ffb5c9671474afc46baa27293975d260
NicholasHamilton27 <nhamil1139@gmail.com>
2025-02-25 09:48:37 -0700

    Update LCSMODEL.m
    corrected derivation in V_mod

--- LCSMODEL.m
+++ LCSMODEL.m
@@ -2,6 +2,6 @@ function [v_mod] = LCSMODEL(r,d,l,theta,w)
 
 beta = asind((d-sind(theta))/l);
 
-v_mod = w * r * (-sind(theta) + tand(beta));
+v_mod = w * r * (-sind(theta) + cosd(theta)*tand(beta));
 
 end
