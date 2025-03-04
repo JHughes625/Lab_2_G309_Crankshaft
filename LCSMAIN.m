e60dea5013b402b612c568047afc9290b6d4430a
NicholasHamilton27 <nhamil1139@gmail.com>
2025-03-04 09:41:00 -0700

    Update LCSMAIN.m

--- LCSMAIN.m
+++ LCSMAIN.m
@@ -4,16 +4,19 @@
 clear all;
 close all;
 clc;
-theta_input = 0:2160; % Angle Input 6 revolutions
-for i = 1:6
-  w_input(i) = mean(dataStruct(i).data.Var4);; % Angular Velocity Input
-end
+%theta_input = 0:2160; % Angle Input 6 revolutions
+filenames = ["Test1_5pt5V", "Test1_6pt5V", "Test1_7pt5V", "Test1_8pt5V", "Test1_9pt5V", "Test1_10pt5V"];
+dataStruct = readData(filenames);
 %matrix_5V = readmatrix("Project_2\Test1_5pt5V");
 
 %plot(matrix_5V(1,:),matrix_5V(5,:));
 
 r_Theo = 7.5; l_Theo = 26; d_Theo = 15; uncertainty_Theo = 0.05; %[cm]
+v_mod = cell(1, 6);
 for i = 1:6
-  v_mod(i) = LCSMODEL(r_Theo,d_Theo,l_Theo,theta_input,w_input(i));
+  v_mod{i} = LCSMODEL(r_Theo,d_Theo,l_Theo,dataStruct(i).data.Var3);
 end
-plot(theta_input,v_mod(i))
+plot(dataStruct(1).data.Var2,v_mod{1})
+hold on
+plot(dataStruct(1).data.Var2, dataStruct(1).data.Var5)
+legend('model', 'data')
