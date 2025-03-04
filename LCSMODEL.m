e2cf0bd07cfb2d3ceb4fb11551af739728e2b34f
NicholasHamilton27 <nhamil1139@gmail.com>
2025-03-04 09:34:00 -0700

    Update LCSMODEL.m
    Dynamic scaling of theta in LCSMODEL based off size of w

--- LCSMODEL.m
+++ LCSMODEL.m
@@ -1,5 +1,5 @@
-function [v_mod] = LCSMODEL(r,d,l,theta,w)
-
+function [v_mod] = LCSMODEL(r,d,l,w)
+theta = linspace(0,2160,length(w));
 beta = asind((d - r .* sind(theta)) / l);
 
 v_mod = w .* r .* (-sind(theta) + cosd(theta).*tand(beta));
