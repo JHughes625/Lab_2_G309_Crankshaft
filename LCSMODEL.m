270857352a1f1df875c7d65855ba92f3956ee621
JHughes625 <97568349+JHughes625@users.noreply.github.com>
2025-03-04 09:01:28 -0700

    Update LCSMODEL.m
    fixed beta value

--- LCSMODEL.m
+++ LCSMODEL.m
@@ -1,7 +1,7 @@
 function [v_mod] = LCSMODEL(r,d,l,theta,w)
 
-beta = asind((d-sind(theta))/l);
+beta = asind((d - r .* sind(theta)) / l);
 
-v_mod = w * r * (-sind(theta) + cosd(theta)*tand(beta));
+v_mod = w .* r .* (-sind(theta) + cosd(theta).*tand(beta));
 
 end
