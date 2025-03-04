beb19931dac4a3e33cb333b1d157451f3cc5c5fb
NicholasHamilton27 <nhamil1139@gmail.com>
2025-03-04 09:02:10 -0700

    Update LCSMAIN.m
    Added means of data to w_input

--- LCSMAIN.m
+++ LCSMAIN.m
@@ -5,14 +5,15 @@ clear all;
 close all;
 clc;
 theta_input = 0:2160; % Angle Input 6 revolutions
-w_input = 1; % Angular Velocity Input
-
+for i = 1:6
+  w_input(i) = mean(dataStruct(i).data.Var4);; % Angular Velocity Input
+end
 %matrix_5V = readmatrix("Project_2\Test1_5pt5V");
 
 %plot(matrix_5V(1,:),matrix_5V(5,:));
 
 r_Theo = 7.5; l_Theo = 26; d_Theo = 15; uncertainty_Theo = 0.05; %[cm]
-
-v_mod = LCSMODEL(r_Theo,d_Theo,l_Theo,theta_input,w_input);
-
-plot(theta_input,v_mod)
+for i = 1:6
+  v_mod(i) = LCSMODEL(r_Theo,d_Theo,l_Theo,theta_input,w_input(i));
+end
+plot(theta_input,v_mod(i))
