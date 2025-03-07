a8a86ffdfda001ffe00a742c57d50d170862e200
NicholasHamilton27 <nhamil1139@gmail.com>
2025-03-07 07:11:44 -0700

    Update LCSMODEL.m
    fixed units error

--- LCSMODEL.m
+++ LCSMODEL.m
@@ -1,7 +1,6 @@
-function [v_mod] = LCSMODEL(r,d,l,theta,w)
-
-beta = asind((d - r .* sind(theta)) / l);
-
-v_mod = w .* r .* (-sind(theta) + cosd(theta).*tand(beta));
-
+function [v_mod] = LCSMODEL(r,d,l,w,theta)
+w = w*pi/180;
+theta = theta*pi/180;
+beta = asin((d - r .* sin(theta)) / l);
+v_mod = (w .* r .* (-sin(theta) - cos(theta).*tan(beta)))*100;
 end
